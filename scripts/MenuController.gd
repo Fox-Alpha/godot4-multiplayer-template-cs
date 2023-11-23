85e40f6b996515f61c1b8a955c23457b15bd8c21
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-11-23 22:48:34 +0100

    Add: Changing windows title

--- scripts/MenuController.gd
+++ scripts/MenuController.gd
@@ -3,12 +3,20 @@ extends Node3D
 var _server_scene = preload("res://scenes/Server.tscn")
 var _client_scene = preload("res://scenes/Client.tscn")
 
-func _on_button_pressed(host:bool):
-		if host:
-			$Control/Label.text = "Server Side"
-			self.add_child(_server_scene.instantiate())
-		else:
-			$Control/Label.text = "Client Side"
-			self.add_child(_client_scene.instantiate())
+@onready var startbuttons: Control = %Buttons
+#@onready var buttons := $Buttons
 
-		$Buttons.queue_free()
+func _on_button_pressed(host:bool):
+	var title : String = "";
+
+	if host:
+		$Control/Label.text = "Server Side"
+		self.add_child(_server_scene.instantiate())
+		title = "Dedicated Server"
+	else:
+		$Control/Label.text = "Client Side"
+		self.add_child(_client_scene.instantiate())
+		title = "Client"
+
+	startbuttons.queue_free()
+	DisplayServer.window_set_title(title)
